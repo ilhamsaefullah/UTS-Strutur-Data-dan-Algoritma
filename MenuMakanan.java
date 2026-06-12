ae8e91751f81ec3b4f9d02aa282657c61de80c99
ilhamsaefullah <ilhamsaefullah0123@gmail.com>
2026-06-12 18:09:31 +0700

    Create MenuMakanan.java

---
+++ MenuMakanan.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class MenuMakanan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Array menu dan harga
+        String[] makanan = {"Soto", "Rawon", "Pecel", "Bakso", "Siomay"};
+        int[] harga = {15000, 20000, 10000, 12500, 25000};
+
+        System.out.print("Berapa jenis makanan yang dipesan? ");
+        int jumlahPesanan = input.nextInt();
+        input.nextLine(); // membersihkan buffer
+
+        String[] pesanan = new String[jumlahPesanan];
+        int[] porsi = new int[jumlahPesanan];
+
+        // Input pesanan
+        for (int i = 0; i < jumlahPesanan; i++) {
+            System.out.print("Masukkan nama makanan ke-" + (i + 1) + " : ");
+            pesanan[i] = input.nextLine();
+
+            System.out.print("Masukkan jumlah porsi : ");
+            porsi[i] = input.nextInt();
+            input.nextLine();
+        }
+
+        int total = 0;
+
+        System.out.println("\nTotal Harga :");
+
+        // Proses perhitungan
+        for (int i = 0; i < jumlahPesanan; i++) {
+            for (int j = 0; j < makanan.length; j++) {
+                if (pesanan[i].equalsIgnoreCase(makanan[j])) {
+                    int subtotal = harga[j] * porsi[i];
+                    total += subtotal;
+
+                    System.out.println("✓ " + makanan[j] +
+                            " @" + harga[j] +
+                            " * " + porsi[i] +
+                            " = " + subtotal);
+                }
+            }
+        }
+
+        System.out.println("Total = " + total);
+    }
+}
